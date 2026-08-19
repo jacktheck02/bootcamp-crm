f4b145e77c19af89d60cc72888c930fe7c1c5e23
Jack Heckenlaible <176347710+jacktheck02@users.noreply.github.com>
2026-08-19 08:11:44 -0700

    Changes to test containers so the build works maybe?

--- server/src/test/java/com/pnc/crm/TestcontainersConfiguration.java
+++ server/src/test/java/com/pnc/crm/TestcontainersConfiguration.java
@@ -20,13 +20,13 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+        return new KafkaContainer(DockerImageName.parse("apache/kafka:3.9.1"));
     }
 
     @Bean
     @ServiceConnection
     PostgreSQLContainer postgresContainer() {
-        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:16"));
     }
 
     @Bean
